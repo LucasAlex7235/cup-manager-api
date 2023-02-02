e134635f77a3db759967b39609a2aa8a61df83e3
Lucas <lucasalex7235@gmail.com>
2023-02-02 13:11:18 -0300

    feat: add routes

--- teams/urls.py
+++ teams/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('teams/', views.TeamView.as_view()),
+    path('teams/<int:team_id>/', views.TeamDetailsView.as_view()),
 ]
--- teams/views.py
+++ teams/views.py
@@ -1,10 +1,10 @@
 from rest_framework.views import APIView, status
 from rest_framework.response import Response
+from rest_framework.request import Request
 from django.db import IntegrityError
 from django.forms.models import model_to_dict
 
 from .models import Team
-import ipdb
 
 
 class TeamView(APIView):
@@ -34,4 +34,36 @@ class TeamView(APIView):
         teams = Team.objects.all()
         teams_dict = [model_to_dict(teams) for teams in teams]
 
-        return Response(teams_dict)
+        return Response(teams_dict, status.HTTP_200_OK)
+
+
+class TeamDetailsView(APIView):
+    def get(self, request: Request, team_id: int):
+        try:
+            team = Team.objects.get(id=team_id)
+        except Team.DoesNotExist:
+            return Response({"message": "Team not found"}, status.HTTP_404_NOT_FOUND)
+        team_dict = model_to_dict(team)
+        return Response(team_dict)
+
+    def patch(self, request: Request, team_id: int):
+        keys_team = [keys.column for keys in Team._meta.get_fields()]
+        validate_schema_key = {key: value for key, value in request.data.items() if key in keys_team}
+
+        try:
+            Team.objects.get(id=team_id)
+        except Team.DoesNotExist:
+            return Response({"message": "Team not found"}, status.HTTP_404_NOT_FOUND)
+
+        Team.objects.filter(id=team_id).update(**validate_schema_key)
+        team_response = model_to_dict(Team.objects.get(id=team_id))
+        return Response(team_response, status.HTTP_200_OK)
+
+    def delete(self, request: Request, team_id: int):
+        try:
+            Team.objects.get(id=team_id)
+        except Team.DoesNotExist:
+            return Response({"message": "Team not found"}, status.HTTP_404_NOT_FOUND)
+
+        Team.objects.filter(id=team_id).delete()
+        return Response(None, status.HTTP_204_NO_CONTENT)
